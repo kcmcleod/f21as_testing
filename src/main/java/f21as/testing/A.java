6b51c7d194aeddce5f333f9d96def2e69ceedcb7
Kenneth McLeod <kcm1@hw.ac.uk>
2019-01-24 12:59:03 +0000

    correcting typo in comments

--- src/main/java/f21as/testing/A.java
+++ src/main/java/f21as/testing/A.java
@@ -5,7 +5,7 @@ package f21as.testing;
 // A demonstrates composition and how to using mocking within testing
 public class A {
 	
-	private B b; // composition = A has a B, e.g., Dog has a collar
+	private B b; // composition = B is part of A, e.g., the heart is part of the human
 
 	public B getB() {
 		return b;
