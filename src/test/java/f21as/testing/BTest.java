897dfeb3ba70ce3b7332a05c24142c6f4169fc88
Kenneth McLeod <kcm1@hw.ac.uk>
2019-01-25 10:05:38 +0000

    simple test for getNumber

---
+++ src/test/java/f21as/testing/BTest.java
@@ -0,0 +1,21 @@
+package f21as.testing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BTest {
+
+    private B b;
+
+    @Before
+    public void setUp() throws Exception {
+        b = new B();
+    }
+
+    @Test
+    public void getNumber() {
+        assertEquals("I expect this to return 0 because it is set to 0 when created", 0, b.getNumber());
+    }
+}
