4023e7398c6d9903b3e4ae5cf5df0169f6ce2f75
Kenneth McLeod <kcm1@hw.ac.uk>
2019-01-25 15:42:16 +0000

    finished setNumber tests

--- src/test/java/f21as/testing/BTest.java
+++ src/test/java/f21as/testing/BTest.java
@@ -1,11 +1,17 @@
 package f21as.testing;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class BTest {
+
+    // this rule is only used for "new_skool_exceptionTest()" below
+    @Rule
+    ExpectedException thrown = ExpectedException.none();
 
     private B b;
 
@@ -32,9 +38,30 @@ public class BTest {
         assertEquals(12, b.getNumber());
     }
 
+    // easiest way to test for exceptions
     @Test(expected = IllegalArgumentException.class)
     public void setNumber_lessThan0_fail() {
         // testing min boundary - 1
+        b.setNumber(-1);
+    }
+
+    @Test
+    public void old_skool_exceptionTest() {
+        try {
+            b.setNumber(-1);
+            fail(); // line ensures test fails if no exception thrown by above line
+        } catch(IllegalArgumentException e) {
+            // test message
+            assertEquals("Value of number must be 0 or more", e.getMessage());
+        }
+    }
+
+
+    @Test
+    public void new_skool_exceptionTest() {
+        // define thrown at top of Tests
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Value of number must be 0 or more");
         b.setNumber(-1);
     }
 }
